17360aaf62c880da833b4a5c283a44224ac491aa
Jiajie Chen <noc@jiegec.ac.cn>
2019-10-19 07:49:30 +0800

    Add pa5-wasm target using wat for wast->wasm translation

--- driver/src/cli.rs
+++ driver/src/cli.rs
@@ -1,6 +1,6 @@
 use driver::{Pa, Alloc};
 use clap::{Arg, App};
-use std::{io, fs, process};
+use std::{io, fs, process, io::Write};
 
 fn main() -> io::Result<()> {
   let matches = App::new("decaf")
@@ -8,14 +8,15 @@ fn main() -> io::Result<()> {
     .arg(Arg::with_name("output").long("output").short("o").takes_value(true))
     .arg(Arg::with_name("target").long("target").short("t").takes_value(true).default_value("pa5"))
     .get_matches();
-  let pa = match matches.value_of("target").unwrap() {
-    t if t.eq_ignore_ascii_case("pa1a") => Pa::Pa1a,
-    t if t.eq_ignore_ascii_case("pa1b") => Pa::Pa1b,
-    t if t.eq_ignore_ascii_case("pa2") => Pa::Pa2,
-    t if t.eq_ignore_ascii_case("pa3") => Pa::Pa3,
-    t if t.eq_ignore_ascii_case("pa4") => Pa::Pa4,
-    t if t.eq_ignore_ascii_case("pa5") => Pa::Pa5,
-    t if t.eq_ignore_ascii_case("pa5wast") => Pa::Pa5Wast,
+  let (pa, wasm) = match matches.value_of("target").unwrap() {
+    t if t.eq_ignore_ascii_case("pa1a") => (Pa::Pa1a, false),
+    t if t.eq_ignore_ascii_case("pa1b") => (Pa::Pa1b, false),
+    t if t.eq_ignore_ascii_case("pa2") => (Pa::Pa2, false),
+    t if t.eq_ignore_ascii_case("pa3") => (Pa::Pa3, false),
+    t if t.eq_ignore_ascii_case("pa4") => (Pa::Pa4, false),
+    t if t.eq_ignore_ascii_case("pa5") => (Pa::Pa5, false),
+    t if t.eq_ignore_ascii_case("pa5wast") => (Pa::Pa5Wast, false),
+    t if t.eq_ignore_ascii_case("pa5wasm") => (Pa::Pa5Wast, true),
     t => {
       eprintln!("invalid target pa: `{}`", t);
       process::exit(1);
@@ -26,10 +27,15 @@ fn main() -> io::Result<()> {
     Ok(p) => p,
     Err(e) => format!("{:?}", e),
   };
+  let result = if wasm {
+    wat::parse_str(result).unwrap()
+  } else {
+    result.into_bytes()
+  };
   if let Some(output) = matches.value_of("output") {
     fs::write(output, result)
   } else {
-    print!("{}", result);
+    io::stdout().write_all(&result)?;
     Ok(())
   }
 }
